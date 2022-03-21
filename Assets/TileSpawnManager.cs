e43971b2ba991293e2a4f21965a28f52241700a1
PTRL483\Anusha.Lakavath <anusha.lakavath@yesgnome.com>
2022-03-21 11:14:53 +0530

    Spawning Tile is Done. If Player Passes the Tiles then it Destroy the Tiles.

---
+++ Assets/TileSpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawnManager : MonoBehaviour
+{
+    public GameObject currentTile;
+    public GameObject[] tilesPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Instantiate(rightTile, currentTile.transform.GetChild(1).position, Quaternion.identity);
+        // Instantiate(forwardTile, currentTile.transform.GetChild(0).position, Quaternion.identity);
+        
+        for(int i=0;i<10;i++)
+        {
+            int k = Random.Range(0,2);
+            currentTile = Instantiate(tilesPrefab[k], currentTile.transform.GetChild(k).position, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+}
